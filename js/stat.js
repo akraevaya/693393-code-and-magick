a1e6fd065add75358ccf2ecabc848ae7894e17f5
Anastasia Kraevaya <42145078+akraevaya@users.noreply.github.com>
2018-11-20 12:52:53 +0300

    Правки и обновления

--- js/stat.js
+++ js/stat.js
@@ -28,6 +28,27 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var getCloudY = function (time, maxTime) {
+  var cloudY = TEXT_HEIGHT * 2 + GAP * 4 + (BAR_CHART_HEIGHT - (BAR_CHART_HEIGHT * time / maxTime));
+  return cloudY;
+};
+
+var getRandomColor = function () {
+  var randomColor = 'rgba(0, 0, 255, ' + Math.random().toFixed(2) + ')';
+  return randomColor;
+};
+
+var getBarColor = function (name) {
+  var barColor;
+  if (name === 'Вы') {
+    barColor = 'rgba(255, 0, 0, 1)';
+  } else {
+    barColor = getRandomColor();
+  }
+  return barColor;
+};
+
+
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#ffffff');
@@ -39,26 +60,13 @@ window.renderStatistics = function (ctx, names, times) {
 
   var maxTime = getMaxElement(times);
 
-  var getCloudY = function () {
-    var cloudY = TEXT_HEIGHT * 2 + GAP * 4 + (BAR_CHART_HEIGHT - (BAR_CHART_HEIGHT * times[i] / maxTime));
-    return cloudY;
-  };
-
-  var getRandomColor = function () {
-    var randomColor = 'rgba(0, 0, 255, ' + Math.random().toString() + ')';
-    if (names[i] === 'Вы') {
-      randomColor = 'rgba(255, 0, 0, 1)';
-    }
-    return randomColor;
-  };
-
   for (var i = 0; i < names.length; i++) {
+    var cloudY = getCloudY(times[i], maxTime);
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), CLOUD_X + (BAR_WIDTH + BAR_GAP) * i, getCloudY() - GAP);
-    ctx.fillStyle = getRandomColor();
-    ctx.fillRect(CLOUD_X + (BAR_WIDTH + BAR_GAP) * i, getCloudY(), BAR_WIDTH, BAR_CHART_HEIGHT * times[i] / maxTime);
-    ctx.fillStyle = '#000';
+    ctx.fillText(Math.round(times[i]), CLOUD_X + (BAR_WIDTH + BAR_GAP) * i, cloudY - GAP);
     ctx.fillText(names[i], CLOUD_X + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT);
+    ctx.fillStyle = getBarColor(names[i]);
+    ctx.fillRect(CLOUD_X + (BAR_WIDTH + BAR_GAP) * i, cloudY, BAR_WIDTH, BAR_CHART_HEIGHT * times[i] / maxTime);
   }
 
 };
