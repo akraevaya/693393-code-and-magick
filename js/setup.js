91fee9b93a65b205f144de62becccc7786508d69
Anastasia Kraevaya <42145078+akraevaya@users.noreply.github.com>
2018-12-02 17:12:27 +0300

    Сделала открытие/закрытие попапа, валидацию, изменение цветов элементов по нажатию

--- js/setup.js
+++ js/setup.js
@@ -1,15 +1,59 @@
 'use strict';
 
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 var WIZARDS_COUNT = 4;
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+// Открытие/закрытие окна настройки персонажа
 
-var similarWizards = userDialog.querySelector('.setup-similar');
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
+var setupClose = setup.querySelector('.setup-close');
+
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+// Похожие персонажи
+
+var similarWizards = setup.querySelector('.setup-similar');
 similarWizards.classList.remove('hidden');
 
 var similarList = similarWizards.querySelector('.setup-similar-list');
@@ -58,3 +102,52 @@ var renderSimilarWizards = function () {
 };
 
 renderSimilarWizards();
+
+// Валидация
+
+var userNameInput = setup.querySelector('.setup-user-name');
+
+userNameInput.addEventListener('invalid', function (_evt) {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из двух символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно быть длиннее 25 символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное для заполнения поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+// Изменение цвета мантии персонажа по нажатию
+
+var setupWizardCoat = setup.querySelector('.setup-wizard .wizard-coat');
+var wizardCoatValue = setup.querySelector('input[name="coat-color"]');
+
+setupWizardCoat.addEventListener('click', function () {
+  var coatColorRandom = getRandomElement(COAT_COLORS);
+  setupWizardCoat.style.fill = coatColorRandom;
+  wizardCoatValue.value = coatColorRandom;
+});
+
+// Изменение цвета глаз персонажа по нажатию
+
+var setupWizardEyes = setup.querySelector('.setup-wizard .wizard-eyes');
+var wizardEyesValue = setup.querySelector('input[name="eyes-color"]');
+
+setupWizardEyes.addEventListener('click', function () {
+  var eyesColorRandom = getRandomElement(EYES_COLORS);
+  setupWizardEyes.style.fill = eyesColorRandom;
+  wizardEyesValue.value = eyesColorRandom;
+});
+
+// Изменение цвета фаерболов по нажатию
+
+var setupFireballColor = setup.querySelector('.setup-fireball-wrap');
+var fireballValue = setup.querySelector('input[name="fireball-color"]');
+
+setupFireballColor.addEventListener('click', function () {
+  var fireballColorRandom = getRandomElement(FIREBALL_COLORS);
+  setupFireballColor.style.backgroundColor = fireballColorRandom;
+  fireballValue.value = fireballColorRandom;
+});
